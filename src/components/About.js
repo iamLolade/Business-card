b99bbb8be0d3bfb0396344c848326167768db214
iamLolade <adeniyiotemade@gmail.com>
2022-01-21 11:42:40 +0100

    Updated about and info componenets

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ function About() {
     return (
         <div className="about">
             <h1>About</h1>
-            <p className="details">I am a frontend developer with a particular interest in making simple, beautiful user interfaces; that leads to a satisfactory user-experience. 
+            <p className="details">I am a MERN stack engineer with a particular interest in making simple, beautiful user interfaces; that leads to a satisfactory user-experience. 
             I try to keep up with security and best practices, and am always looking for new things to learn.</p>
         </div>
     )
