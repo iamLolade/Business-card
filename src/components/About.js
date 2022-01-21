0ab5da6e59e5a6bae5ad84a2ce20f3a206aad9b4
iamLolade <adeniyiotemade@gmail.com>
2022-01-21 17:13:33 +0100

    Final update

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,8 @@ function About() {
     return (
         <div className="about">
             <h1>About</h1>
-            <p className="details">I am a MERN stack engineer with a particular interest in making simple, beautiful user interfaces; that leads to a satisfactory user-experience. 
-            I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <p className="details">I am a MERN stack engineer with a particular interest in making simple, beautiful user interfaces; that lead to a satisfactory user experience. 
+            I deliver working products, keep up with security and best practices, and am always in search of new technologies to learn.</p>
         </div>
     )
 }
