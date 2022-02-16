7fb31475efba04dfac8a22fe9354c188146dbd18
iamLolade <adeniyiotemade@gmail.com>
2022-02-16 01:57:42 +0100

    Deploy ready

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,7 +6,7 @@ function Info() {
         <div className="header">
             <img src={headShot} alt="Picture of Ololade" />
             <h2 className="name">Ololade Otemade</h2>
-            <p className="title">MERN Stack Engineer</p>
+            <p className="title">Software Engineer</p>
             <div className="contact-buttons">
                 <a href="mailto:adeniyiotemade@gmail.com"><button type="submit"><img src={emailLogo} alt="Email-logo"/>Email</button></a>
                 <a href="https://www.linkedin.com/in/ololade-otemade-72959613a/" target="_blank"><button type="submit" className="button2"><img src={linkedInLogo} alt="LinkenIn-logo"/>LinkenIn</button></a>
